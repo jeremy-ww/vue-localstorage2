dc49d714b5f2b7b740ec2b66c41a089155793f77
伍程凯 <wuchengkai0@gmail.com>
2018-03-06 16:58:28 +0800

    chore(example): reduce docs static file size

--- examples/index.js
+++ examples/index.js
@@ -1,11 +1,16 @@
+import Button from 'element-ui/lib/button'
+import Input from 'element-ui/lib/input'
 import Tag from 'element-ui/lib/tag'
 import localStorage2 from '../'
 import Demo from './Demo'
 import Vue from 'vue'
 
+import 'element-ui/lib/theme-chalk/button.css'
+import 'element-ui/lib/theme-chalk/input.css'
 import 'element-ui/lib/theme-chalk/tag.css'
 
-Vue.use(Tag)
+;[Button, Tag, Input].forEach(component => Vue.use(component))
+
 Vue.use(localStorage2)
 
 new Vue({
